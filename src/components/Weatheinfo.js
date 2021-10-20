9f7dc8f95b5129d6f7e9d0ea3fa900885452e664
AlbeTorres <albertocorreoficial@gmail.com>
2021-10-20 11:22:25 -0400

    test commit

--- src/components/Weatheinfo.js
+++ src/components/Weatheinfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Weatheinfo = () => {
     return (
         <div>
-            <h3>Info del clima buti chama</h3>
+            <h3>Clima info</h3>
         </div>
     )
 }
