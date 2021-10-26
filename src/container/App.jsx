0db06cf7271c7c7d375c8791e4137eb93845de34
AlbeTorres <albertocorreoficial@gmail.com>
2021-10-26 09:54:52 -0400

    fecth data good async ok

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,33 +1,57 @@
 import React from 'react';
+import { useEffect } from 'react';
 import Weatheinfo from '../components/Weatheinfo';
 import WeatherForm from '../components/WeatherForm';
+import { WEATHER_KEY } from '../key';
 
 
 
-const getWeather= (e) =>{
-    e.preventDefault();
 
-    const {city,country}= e.target.elements;
-    const countryValue = country.value;
-    const cityValue = city.value;
 
-    console.log(`${cityValue} ${countryValue} `)
-}
+const App = () =>{
+    const API_URL=`http://api.openweathermap.org/data/2.5/weather?q=Cuba&appid=${WEATHER_KEY}`
 
-const App = () =>(
-    <div className='App'>
-        <div className='container p-4'>
-            <div className='row'>
-                <div className='col-md-4 mx-auto'>
-                    <WeatherForm getWeather={getWeather}/>
-                    <Weatheinfo/>
+    const getWeather= (e) =>{
+        e.preventDefault();
+    
+        const {city,country}= e.target.elements;
+        const countryValue = country.value;
+        const cityValue = city.value;
+    
+        const API_URL=`http://api.openweathermap.org/data/2.5/weather?q=${countryValue}&appid=${WEATHER_KEY}`
+    
+        console.log(`${cityValue} ${countryValue} ${API_URL}`)
+    }
 
+    useEffect(()=>{
+        const fetchWeather =async()=>{
+
+            const response= await fetch(API_URL)
+            const data = await response.json()
+
+            console.log(data)
+        }
+
+        fetchWeather()
+    },[])
+    
+
+    return(
+        <div className='App'>
+            <div className='container p-4'>
+                <div className='row'>
+                    <div className='col-md-4 mx-auto'>
+                        <WeatherForm getWeather={getWeather}/>
+                        <Weatheinfo/>
+    
+                    </div>
+    
                 </div>
-
             </div>
+            
         </div>
-        
-    </div>
-);
+    );
+}
+    
 
 export default App;
---
+++ src/key.js
@@ -0,0 +1,3 @@
+module.exports ={
+    WEATHER_KEY: '2aba2b22a9037f384bf812f56b4c6a76'
+}
